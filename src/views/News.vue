4b3471480264723dd2abc34d5e70752e97cfca75
ryan <librazak@gmail.com>
2019-09-24 23:07:28 +0800

    updateNews

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,51 +1,41 @@
 <template >
-<div class="container-fluid">
-    <h1 class="text-center my-3">最新消息</h1>
-<!-- 
-    <div class="card" style="width: 18rem;">
-            <img src="https://fakeimg.pl/400x300/" class="card-img-top" alt="picture">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div> -->
+<div>
+<h1 class="text-center my-3">最新消息</h1>
 
-    <div class="card-deck">
-  <div class="card">
-    <img class="card-img-top" src="https://www.vscinemas.com.tw/vsweb/upload/event/event_20190923003.jpg" alt="Card image cap">
+  <div class="card-deck">
+  <div class="card mt-2 ml-5">
+    <img class="card-img-top" src="" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">《台南FOCUS限定》打卡同樂趣 一起FUN電影！</h5>
-      <p class="card-text">憑台南FOCUS威秀影城手機打卡畫面+當日電影票或消費發票，即可兌換「威秀寶寶萬用桶」乙個！數量有限，送完為止！</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted"><a href="#" class="btn btn-primary">Go somewhere</a></small>
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
-  <div class="card">
+  <div class="card mt-2">
     <img class="card-img-top" src="" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
-  <div class="card">
-    <img class="card-img-top" src="">
+  <div class="card mt-2 mr-5">
+    <img class="card-img-top" src="" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
 </div>
 
- 
-       
+
+
+
+
+
+
+
 </div>
 </template>
 
