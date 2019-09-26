e178d787b023b69ad47df97214538b2a982cb895
ryan <librazak@gmail.com>
2019-09-26 19:22:10 +0800

    news update

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,37 +1,258 @@
 <template >
-<div>
-<h1 class="text-center my-3">最新消息</h1>
+<div id="app">
 
-  <div class="card-deck">
-  <div class="card mt-2 ml-5">
-    <img class="card-img-top" src="" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card mt-2">
-    <img class="card-img-top" src="" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card mt-2 mr-5">
-    <img class="card-img-top" src="" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+<h1 class="text-center my-4">最新消息</h1>
+
+  <ul class="eventList">
+      <li>
+        <figure style="height:250px;">
+          <a href="#" style="height:250px;">
+              <img src="..\assets\event_2019_1.jpg" 
+              alt="《台南FOCUS限定》打卡同樂趣 一起FUN電影！" 
+              title="《台南FOCUS限定》打卡同樂趣 一起FUN電影！" 
+              style="height:100%; width:auto; margin-left:0px;"/>
+          </a>
+        </figure>
+        <section class="infoArea">
+        <h5>
+          <a href="#" style="height:250px;">
+            《台南FOCUS限定》打卡同樂趣 一起FUN電影！
+          </a>
+        </h5>
+        <time>2019/10/03 ~ 2019/10/31</time>
+        </section>
+      </li>
+
+
+
+      <li>
+      <figure style="height:250px;">
+          <a href="#" style="height:250px;">
+              <img src="..\assets\event_2019_2.jpg" 
+              alt="9月電影看不完 點數拿不完" 
+              title="9月電影看不完 點數拿不完" 
+              style="height:100%; width:auto; margin-left:0px;"/>
+          </a>
+        </figure>
+        <section class="infoArea">
+        <h5>
+          <a href="#" style="height:250px;">
+            9月電影看不完 點數拿不完
+          </a>
+        </h5>
+        <time>2019/09/06 ~ 2019/09/30</time>
+        </section>
+      </li>
+
+
+
+      <li>
+        <figure style="height:250px;">
+          <a href="#" style="height:250px;">
+              <img src="..\assets\event_2019_3.jpg" 
+              alt="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              title="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              style="height:100%; width:auto; margin-left:0px;"/>
+          </a>
+        </figure>
+        <section class="infoArea">
+        <h5>
+          <a href="#" style="height:250px;">
+            《林口限定》Mappa獨家加碼。 iShow卡友享好禮
+          </a>
+        </h5>
+        <time>2019/09/20 ~ 2019/10/06</time>
+        </section>
+      </li>
+
+
+
+
+      <li>
+        <figure style="height:250px;">
+          <a href="#" style="height:250px;">
+              <img src="..\assets\event_2019_3.jpg" 
+              alt="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              title="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              style="height:100%; width:auto; margin-left:0px;"/>
+          </a>
+        </figure>
+        <section class="infoArea">
+        <h5>
+          <a href="#" style="height:250px;">
+            《林口限定》Mappa獨家加碼。 iShow卡友享好禮
+          </a>
+        </h5>
+        <time>2019/09/20 ~ 2019/10/06</time>
+        </section>
+      </li>
+
+
+
+      <li>
+        <figure style="height:250px;">
+          <a href="#" style="height:250px;">
+              <img src="..\assets\event_2019_3.jpg" 
+              alt="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              title="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              style="height:100%; width:auto; margin-left:0px;"/>
+          </a>
+        </figure>
+        <section class="infoArea">
+        <h5>
+          <a href="#" style="height:250px;">
+            《林口限定》Mappa獨家加碼。 iShow卡友享好禮
+          </a>
+        </h5>
+        <time>2019/09/20 ~ 2019/10/06</time>
+        </section>
+      </li>
+
+
+      <li>
+        <figure style="height:250px;">
+          <a href="#" style="height:250px;">
+              <img src="..\assets\event_2019_3.jpg" 
+              alt="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              title="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              style="height:100%; width:auto; margin-left:0px;"/>
+          </a>
+        </figure>
+        <section class="infoArea">
+        <h5>
+          <a href="#" style="height:250px;">
+            《林口限定》Mappa獨家加碼。 iShow卡友享好禮
+          </a>
+        </h5>
+        <time>2019/09/20 ~ 2019/10/06</time>
+        </section>
+      </li>
+
+      <li>
+        <figure style="height:250px;">
+          <a href="#" style="height:250px;">
+              <img src="..\assets\event_2019_3.jpg" 
+              alt="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              title="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              style="height:100%; width:auto; margin-left:0px;"/>
+          </a>
+        </figure>
+        <section class="infoArea">
+        <h5>
+          <a href="#" style="height:250px;">
+            《林口限定》Mappa獨家加碼。 iShow卡友享好禮
+          </a>
+        </h5>
+        <time>2019/09/20 ~ 2019/10/06</time>
+        </section>
+      </li>
+
+
+      <li>
+        <figure style="height:250px;">
+          <a href="#" style="height:250px;">
+              <img src="..\assets\event_2019_3.jpg" 
+              alt="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              title="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              style="height:100%; width:auto; margin-left:0px;"/>
+          </a>
+        </figure>
+        <section class="infoArea">
+        <h5>
+          <a href="#" style="height:250px;">
+            《林口限定》Mappa獨家加碼。 iShow卡友享好禮
+          </a>
+        </h5>
+        <time>2019/09/20 ~ 2019/10/06</time>
+        </section>
+      </li>
+
+
+      <li>
+        <figure style="height:250px;">
+          <a href="#" style="height:250px;">
+              <img src="..\assets\event_2019_3.jpg" 
+              alt="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              title="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              style="height:100%; width:auto; margin-left:0px;"/>
+          </a>
+        </figure>
+        <section class="infoArea">
+        <h5>
+          <a href="#" style="height:250px;">
+            《林口限定》Mappa獨家加碼。 iShow卡友享好禮
+          </a>
+        </h5>
+        <time>2019/09/20 ~ 2019/10/06</time>
+        </section>
+      </li>
+
+
+      
+      <li>
+      
+        <figure style="height:260px;">
+          <a href="#"  style="height:260px;" data-toggle="modal" data-target=".bd-example-modal-lg">
+              <img src="..\assets\event_2019_3.jpg" 
+              alt="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              title="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" 
+              style="height:100%; width:auto; margin-left:0px;"/>
+          </a>
+        </figure>
+        <section class="infoArea">
+        <h5>
+          <a href="#" style="height:260px;" data-toggle="modal" data-target=".bd-example-modal-lg">
+            《林口限定》Mappa獨家加碼。 iShow卡友享好禮
+          </a>
+        </h5>
+        <time>2019/09/20 ~ 2019/10/06</time>
+        </section>
+        
+
+        <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+        <div class="modal-content" style="text-align:left;">
+              <p>《林口限定》Mappa獨家加碼。 iShow卡友享好禮</p>
+              <p>開始日期 : 2019/09/20</p>
+              <p>截止日期 : 2019/10/06</p>
+              <p>活動地點 : 林口MITSUI OUTLET PARK威秀影城</p>
+              <p>活動辦法 : 於影城臨櫃使用iShow卡單筆儲值1,000元以上，並購買Mappa影廳任一場次電影票，即可立即獲得加碼好禮。</p>
+
+
+              <p>09/20-09/29「2019世界音樂節單日票」乙張</p>
+              <p>09/30-10/06 「歐舒丹護手霜」乙條</p>
+
+
+
+            <p>注意事項:
+            <p>1. 本活動僅適用於林口MITSUI OUTLET PARK威秀影城。</p>
+            <p>2. 贈品圖片僅供參考，規格以實際贈品為主。</p>
+            <p>3. 贈品一人一卡限兌乙次，數量有限、送完為止。</p>
+            <p>4. 威秀影城保有活動最終解釋權。</p>
+
+
+
+
+
+            </div>
+          </div>
+        </div>
+      </li>
+
+  </ul>
+
+
+
+
+
+  <div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      ...
     </div>
   </div>
 </div>
-
-
-
-
 
 
 
@@ -39,4 +260,59 @@
 </div>
 </template>
 
+<style>
 
+  .eventList ul{
+         list-style-type:none;
+         margin:0;
+         padding: 0;
+         display: block;
+         width: 900px;
+         margin: 0 auto; 
+         border: 1px solid red;
+         text-decoration: none;
+  
+      }
+      .eventList li{
+          float: left;
+          
+          display: block;
+          width: 230px;
+          height: 400px;
+          text-align: center;
+          margin: 0 auto;
+          margin: 30px;
+      }
+      figure{
+          display: block;
+          height:400px;
+          margin: 0 auto;
+          padding-top:40px;
+      }
+      figure a{
+          
+          
+      } 
+      figure a img{
+          height:100%; 
+          width:auto; 
+          margin-left:0px;
+          margin:0 auto;
+      }
+      .eventList li:hover{
+        background-color: #d9d9d9;
+        text-decoration: none;
+
+      }
+      .infoArea{
+        padding-top:30px;
+      }
+      time{
+        padding-top:50px;
+      }
+      
+</style>
+
+
+
+  
