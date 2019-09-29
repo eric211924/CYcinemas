ddaed6a62095a0738c6302eb20a79b35e3857fcf
librazak <librazak@gmail.com>
2019-09-29 10:32:54 +0800

    api test2

--- src/views/frontEnd/News.vue
+++ src/views/frontEnd/News.vue
@@ -1,5 +1,5 @@
 <template >
-  <div id="app">
+  <div>
     <h1 class="text-center my-4">最新消息</h1>
 
     <ul class="eventList">
@@ -22,8 +22,6 @@
           <time>2019/10/03 ~ 2019/10/31</time>
         </section>
       </li>
-
-
 
       <li>
         <figure style="height:250px;">
@@ -160,45 +158,68 @@
 
       <li>
         <figure>
-          <a href="#" style="height:250px;">
-            <img
-              src="..\..\assets\event_2019_3.jpg"
-              alt="《林口限定》Mappa獨家加碼。 iShow卡友享好禮"
-              title="《林口限定》Mappa獨家加碼。 iShow卡友享好禮"
-              style="height:100%; width:auto; margin-left:0px;"
-            />
+          <a href="#" data-toggle="modal" data-target=".bd-example-modal-lg">
+            <img src="..\..\assets\event_2019_3.jpg" alt="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" title="《林口限定》Mappa獨家加碼。 iShow卡友享好禮" />
           </a>
         </figure>
         <section class="infoArea">
           <h5>
-            <a href="#" style="height:250px;">《林口限定》Mappa獨家加碼。 iShow卡友享好禮</a>
+            <a href="#" data-toggle="modal" data-target=".bd-example-modal-lg">《林口限定》Mappa獨家加碼。 iShow卡友享好禮</a>
           </h5>
-          <time>2019/09/20 ~ 2019/10/06</time>
+          <time>2019/10/01~2019/10/31</time>
         </section>
-        
+
+        <div
+          class="modal fade bd-example-modal-lg"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="myLargeModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content" style="text-align:left; padding:40px">
+              <h3>《林口限定》Mappa獨家加碼。 iShow卡友享好禮</h3>
+              <p>
+                <i class="fa fa-calendar" aria-hidden="true"></i>
+                開始日期 : 2019/10/01
+              </p>
+              <p>
+                <i class="fa fa-calendar" aria-hidden="true"></i>
+                截止日期 : 2019/10/31
+              </p>
+              <p>
+                <i class="fa fa-map-marker" aria-hidden="true"></i> 
+                活動地點 : 林口MITSUI OUTLET PARK威秀影城
+              </p>
+              <p>
+                <i class="fa fa-file-text-o" aria-hidden="true"></i> 
+                活動辦法 : 於影城臨櫃使用iShow卡單筆儲值1,000元以上，並購買Mappa影廳任一場次電影票，即可立即獲得加碼好禮。
+              </p>
+
+              <p>09/20-09/29「2019世界音樂節單日票」乙張</p>
+              <p>09/30-10/06 「歐舒丹護手霜」乙條</p>
+
+              <p>注意事項:</p>
+              <p>1. 本活動僅適用於林口MITSUI OUTLET PARK威秀影城。</p>
+              <p>2. 贈品圖片僅供參考，規格以實際贈品為主。</p>
+              <p>3. 贈品一人一卡限兌乙次，數量有限、送完為止。</p>
+              <p>4. 威秀影城保有活動最終解釋權。</p>
+            </div>
+          </div>
+        </div>
       </li>
--------------------------------------------------------------------------------------------------------------
       <li v-for="info in infos">
         <figure>
           <a href="#" data-toggle="modal" data-target=".bd-example-modal-lg">
-            <img
-              :src="info.news_img_normal_url"
-              :alt="info.news_title"
-              :title="info.news_title"
-            />
+            <img :src="info.news_img_normal_url" :alt="info.news_title" :title="info.news_title" />
           </a>
         </figure>
         <section class="infoArea">
           <h5>
-            <a
-              href="#"
-              data-toggle="modal"
-              data-target=".bd-example-modal-lg"
-            >{{news_time}}</a>
+            <a href="#" data-toggle="modal" data-target=".bd-example-modal-lg">{{news_time}}</a>
           </h5>
           <time>{{info.news_time}}</time>
         </section>
-
 
         <div
           class="modal fade bd-example-modal-lg"
@@ -211,10 +232,12 @@
             <div class="modal-content" style="text-align:left; padding:40px">
               <h3>{{info.title}}</h3>
               <p>
-                <i class="fa fa-calendar" aria-hidden="true"></i> 開始日期 : {{info.news_time}}
+                <i class="fa fa-calendar" aria-hidden="true"></i>
+                開始日期 : {{info.news_time}}
               </p>
               <p>
-                <i class="fa fa-calendar" aria-hidden="true"></i> 截止日期 : {{info.news_time}}
+                <i class="fa fa-calendar" aria-hidden="true"></i>
+                截止日期 : {{info.news_time}}
               </p>
               <p>
                 <i class="fa fa-map-marker" aria-hidden="true"></i> 活動地點 : 林口MITSUI OUTLET PARK威秀影城
@@ -234,7 +257,6 @@
             </div>
           </div>
         </div>
-
       </li>
     </ul>
 
@@ -249,7 +271,6 @@
         <div class="modal-content">...</div>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -281,8 +302,8 @@ figure {
   margin: 0 auto;
   padding-top: 40px;
 }
-figure a{
-  height:250px;
+figure a {
+  height: 250px;
 }
 figure a img {
   height: 100%;
@@ -290,8 +311,8 @@ figure a img {
   margin-left: 0px;
   margin: 0 auto;
 }
-h5 a{
-  height:250px;
+h5 a {
+  height: 250px;
 }
 .eventList li:hover {
   background-color: #d9d9d9;
@@ -311,12 +332,14 @@ export default {
     return { infos: null };
   },
   mounted() {
-    const _this=this;
-    this.axios.get("https://cy-cinemas.ml/news/get").then(response => {
-      _this.infos = response.data;
-      console.log(_this.infos);
-
-    }).catch(function (error) { 
+    const _this = this;
+    this.axios
+      .get("https://cy-cinemas.ml/news/get")
+      .then(response => {
+        _this.infos = response.data;
+        console.log(_this.infos);
+      })
+      .catch(function(error) {
         console.log(error);
       });
   },
