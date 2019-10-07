def905fb60518e803c269029fb00be7afe7982e7
e6320123 <ben780618@gmail.com>
2019-10-07 18:54:15 +0800

    check input

--- src/views/frontEnd/Detail.vue
+++ src/views/frontEnd/Detail.vue
@@ -204,13 +204,13 @@
           <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">error</h5>
+                <h5 class="modal-title" id="exampleModalCenterTitle">資料錯誤</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
               <div class="modal-body"> 
-                error
+                請確認輸入資料無誤
               </div> 
               <div class="modal-body">
               <div class="container"> 
