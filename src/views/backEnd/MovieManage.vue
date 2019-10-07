ef84e530737187c94e3c4255d12f8c0a9153c426
Louis3240 <rt4827488@yahoo.com.tw>
2019-10-07 22:06:03 +0800

    add crawl button to backEnd and some change to movieManage

--- src/views/backEnd/MovieManage.vue
+++ src/views/backEnd/MovieManage.vue
@@ -9,13 +9,73 @@
       @click.prevent="action = '新增'"
     >新增 +</button>
 
-    <button class="btn btn-primary mt-4 ml-3" @click.prevent="crawl">
-      爬蟲
-      <i class="fa fa-hand-rock-o" aria-hidden="true"></i>
-    </button>
+    <!-- <div class="row mt-3">
+      <div class="col-12 col-md-6 col-lg-3 mb-3" v-for="(item, index) in moviesData" :key="index">
+        <a href data-toggle="modal" data-target="#showMovieData" @click.prevent="showMovie = item">
+          <div class="card">
+            <img :src="item.img_thumbs_url" alt class="card-img-top" />
+            <div class="card-body">
+              <h6 class="card-title overflow-hidden w-100">{{ item.title }}</h6>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div> -->
+
+    <!-- Modal -->
+    <!-- <div
+      class="modal fade"
+      id="showMovieData"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="showMovieData"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered modal-xl modal-md" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" id="showMovieData">{{ showMovie.title }}</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row w-100">
+              <div class="col-4">
+                <img :src="showNews.img_thumbs_url" class="img-thumbnail w-100" alt />
+              </div>
+              <div class="col-8">
+                <p v-if="showNews.start_time != '0000-00-00'">起始時間 : {{ showNews.start_time }}</p>
+                <p v-if="showNews.end_time != '0000-00-00'">結束時間 : {{ showNews.end_time }}</p>
+                <p>{{ showNews.content }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="card-footer">
+            <div class="btn-group my-1 d-flex justify-cotent-center">
+              <button
+                class="btn btn-warning btn-sm"
+                data-toggle="modal"
+                data-target="#NewsForm"
+                data-dismiss="modal"
+                @click.prevent="action = '修改'; getNewsData(showNews.id)"
+              >修改</button>
+              <button
+                class="btn btn-danger btn-sm"
+                data-toggle="modal"
+                data-target="#deleteModal"
+                @click.prevent="setId = showNews.id"
+                data-dismiss="modal"
+              >刪除</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <DeleteModal @delete="deleteNews" @clear="cleanData"></DeleteModal> -->
 
     <!-- 電影表單 -->
-    <div
+    <!-- <div
       class="modal fade"
       id="MoviesForm"
       tabindex="-1"
@@ -86,7 +146,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div>-->
     <Loading v-if="isLoading"></Loading>
 
     <!-- 最外層結束 -->
@@ -104,31 +164,20 @@ export default {
   },
   data() {
     return {
-      test: "",
-      isLoading: false // 顯示 Loading 圖示直到資料準備好
+      isLoading: false
     };
   },
-  methods: {
-    crawl() {
-      this.isLoading = true;
-      this.axios
-        .get(`${this.$api}/crawl.php`)
-        .then(response => {
-          this.test = response.data;
-          this.isLoading = false;
-          this.$toasted.success("爬蟲成功", {
-            theme: "bubble",
-            duration: 3000
-          });
-        })
-        .catch(function(){
-          this.isLoading = false;
-          this.$toasted.error("爬蟲失敗", {
-            theme: "bubble",
-            duration: 3000
-          });
-        });
-    }
-  }
+  methods: {}
 };
 </script>
+
+
+
+<style lang="scss" scoped>
+.card-title {
+  height: 30px;
+  line-height: 30px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
