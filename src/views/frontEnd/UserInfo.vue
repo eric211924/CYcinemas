2a2542bcc8ce52fe9f35ffca57dbd483545cf946
Paul <paul1994331@gmail.com>
2019-10-08 18:12:36 +0800

    edit reg. form and user/userinfo

--- src/views/frontEnd/UserInfo.vue
+++ src/views/frontEnd/UserInfo.vue
@@ -1,23 +1,42 @@
 <template>
     <div>
         <h1 class="text-center my-5">會員資料</h1>
-        Name: {{ name }}
-        Account: {{ account }}
-        Password: {{ password }}
-        Email: {{ email }}
-        Phone: {{ phone }}
         <hr>
-        姓名: XXXX (不可改)
+        姓名: {{ name }}
         <br><br>
-        帳號: XXXX (不可改)
+        帳號: {{ account }}
         <br><br>
-        密碼: XXXX (可改)
-        <input type="submit" value="修改密碼">
+        密碼: ••••••••••
         <br><br>
-        Email: XXXX@XXXXX.XXX (可改)
+        Email: {{ email }}
         <br><br>
-        電話: 12345678 (可改)
+        電話: {{ phone }}
+        <br><br>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#edit" @click="editData">修改會員資料</button>
+
+        <!-- 會員資料修改視窗 -->
+        <div class="modal fade" id="edit">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalCenterTitle">修改會員資料</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        test
+                    </div>
+                    <div class="modal-footer">
+                        footer
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
+
+    
 </template>
 
 <script>
@@ -35,17 +54,35 @@ export default {
         this.checkMember();
     },
     methods: {
+        editData() {
+            console.log("EDIT!");
+        },
         checkMember() {
-            console.log("OK");
-            var getAccount = JSON.parse(localStorage.getItem("allUser")); // 取得會員清單
-            var logAccount = localStorage.getItem('logAccount'); // 取得登入帳號
-            var accIsExist = getAccount.find(getAccount => getAccount.account == logAccount); // 取得目前登入會員資料
-
-            this.name = accIsExist.name;
-            this.account = accIsExist.account;
-            this.password = accIsExist.password;
-            this.email = accIsExist.email;
-            this.phone = accIsExist.phone;
+            if (sessionStorage.getItem('status') == 'login') {
+                var nowAcc = sessionStorage.getItem('nowAcc');
+                var _this = this;
+                var formData = new FormData();
+                formData.append('account', nowAcc);
+                this.axios.post('https://cy-cinemas.ml/members/showUserData', formData)
+                    .then(function (response) {
+                        // 傳目前登入的帳號過去找 找回所有資料 顯示於頁面上
+                        var result = response.data;
+                        _this.name = result[0].name;
+                        _this.account = result[0].account;
+                        _this.password = result[0].password;
+                        _this.email = result[0].email;
+                        _this.phone = result[0].phone;
+                        // _this.$toasted.success("會員資料取得成功", {
+                        //     theme: 'bubble',
+                        //     duration: 3000
+                        // });
+                    }).catch(function (error) {
+                        _this.$toasted.error(error, {
+                            theme: 'bubble',
+                            duration: 3000
+                        });
+                    });
+            }   
         }
     }
 }
