fac70e3a6f5f580cc6c4e560ce43b7e83ba6dac1
ryan <librazak@gmail.com>
2019-10-08 21:08:09 +0800

    movie

--- src/views/frontEnd/Movie.vue
+++ src/views/frontEnd/Movie.vue
@@ -21,7 +21,7 @@
               </a>
               <p
                 class="card-text"
-              >由布萊德彼特飾演一名航太工程師，為了尋找多年前啟程外太空尋找外星智慧生物，失蹤在茫茫宇宙的父親，他不惜跨越宇宙星系間，只為了尋找父親可能 仍生還的下落。</p>
+              ></p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -51,7 +51,7 @@
               <h5 class="card-title">全面攻佔3</h5>
               <p
                 class="card-text"
-              >這次恐怖分子頭號目標，竟是最強特務-麥克班寧！《全面攻佔3：天使救援》故事時間設定在倫敦淪陷的兩年後，當年的副總統-艾倫已經變成美國總統，麥克班寧晉升為特勤局局長。恐怖份子這次盯上麥克，陷害他成為謀殺總統的通緝犯，被全球通緝！</p>
+              ></p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -71,7 +71,6 @@
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title">返校</h5>
-              <p class="card-text">1962年寂寥而肅殺的時代，翠華中學高三生方芮欣，意外與輔導老師張明暉相戀，在問題叢生的家庭及學校中，他成為她唯一的出路。</p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -79,6 +78,7 @@
           </div>
         </div>
       </div>
+
       <div class="card mb-3" style="max-width: 540px;">
         <div class="row no-gutters">
           <div class="col-md-4">
@@ -93,7 +93,7 @@
               <h5 class="card-title">花椒之味</h5>
               <p
                 class="card-text"
-              >在旅行社工作的夏如樹得知父親突如其來的死訊的同時，發現原來自己在台北、重慶兩地各有一個同父異母的妹妹夏如枝及夏如果。來自三個不同地域、性格迴異的三姐妹，在父親的葬禮上首次碰面，明明陌生却有著血緣的牽絆。</p>
+              ></p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -101,6 +101,7 @@
           </div>
         </div>
       </div>
+
       <div class="card mb-3" style="max-width: 540px;">
         <div class="row no-gutters">
           <div class="col-md-4">
@@ -115,7 +116,7 @@
               <h5 class="card-title">我家有個開心農場</h5>
               <p
                 class="card-text"
-              >親手打造一座沒有極限的農場！只為了堅持對愛狗的承諾，約翰與茉莉這對瘋狂夫妻索性來去鄉下開農場！本片歷時十年追蹤，跟著這兩位瘋狂的夢想家和他們的愛狗塔德。</p>
+              >。</p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -137,7 +138,7 @@
               <h5 class="card-title">天氣之子</h5>
               <p
                 class="card-text"
-              >高一那年夏天，帆高離開位在離島的家鄉，獨自來到東京，拮据的生活迫使他不得不找份工作，最後來到一間專門出版奇怪超自然刊物的出版社擔任寫手。</p>
+              ></p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -164,7 +165,7 @@
               <h5 class="card-title">安眠醫生</h5>
               <p
                 class="card-text"
-              >史蒂芬金小說改編，經典電影「鬼店」續集！經過這麼多年，丹尼的依舊為童年時期遭遇的可怕事件留下創傷，當他遇到艾柏拉這位勇敢少女時，他平靜的生活完全破碎</p>
+              ></p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -186,7 +187,7 @@
               <h5 class="card-title">冰雪奇緣2</h5>
               <p
                 class="card-text"
-              >《冰雪奇緣2》故事描述艾倫戴爾王國在女王艾莎帶領之下欣欣向榮，之前相敬如”冰”的姊妹也化解了心結，正當一切美好之際，艾莎卻聽到來自遠方的呼喚。</p>
+              ></p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -208,7 +209,7 @@
               <h5 class="card-title">只有我能喜歡你</h5>
               <p
                 class="card-text"
-              >★《模犯生》「阿班」查農桑提納同庫甩偶像包袱，變身幼稚中二小屁孩！霸氣十足的阿杜，從小就視小他一歲的漂亮小姑姑小琳為心目中的女神，因此在學校中只要誰敢靠近她，他便會想盡各種辦法搞破壞！</p>
+              ></p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -230,7 +231,7 @@
               <h5 class="card-title">布魯克林孤兒</h5>
               <p
                 class="card-text"
-              >在《布魯克林孤兒》中，艾德華諾頓飾演主角：萊諾艾斯洛，一個沒沒無聞又患有妥瑞症的私家偵探，提攜他的恩人與唯一的朋友法蘭克敏納遭人刺殺，憑藉著極有限的線索、超乎常人的執著、毅力，他一步一步地揭開隱藏在陰影下被層層保護的機密，</p>
+              ></p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -252,7 +253,7 @@
               <h5 class="card-title">最後一滴血</h5>
               <p
                 class="card-text"
-              >遠離血腥戰場的英雄藍波，深陷創傷症候群的困擾，獨居在偏僻的農場中，過著人生中最黑暗的時刻。但當藍波得知朋友女兒遭到墨西哥毒販的綁架，他立刻展開調查與救援。 這次他將再次運用精湛的機關與戰鬥技巧，完成最後一次營救任務！</p>
+              ></p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -274,7 +275,7 @@
               <h5 class="card-title">STAR WARS 天行者的崛起</h5>
               <p
                 class="card-text"
-              >上集中拯救了反抗軍的「天行者路克」這次將以絕地英靈之姿回歸。原力覺醒的「芮」終於以『絕地』的身分跟反抗軍老夥伴們重逢。另一方面，由「凱羅忍」帶領的第一軍團所造成的威脅似乎不曾變少</p>
+              ></p>
               <p class="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
@@ -282,6 +283,9 @@
           </div>
         </div>
       </div>
+
+
+      
     </div>
   </div>
 </template>
